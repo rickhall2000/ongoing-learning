fba4ea5043f3b6d2022f715eefa53469d1a0c666
Rick Hall <rickhall2000@hotmail.com>
2023-02-06 11:26:13 -0500

    iterators it improve mingrep

--- Rust/the_rust_programming_language/minigrep/src/lib.rs
+++ Rust/the_rust_programming_language/minigrep/src/lib.rs
@@ -9,12 +9,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -43,31 +51,23 @@ pub fn search<'a>(
     contents: &'a str,
 ) -> Vec<&'a str> {
 
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query){
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()    
 }
 
 pub fn search_case_insenstive<'a>(
     query: &str,
     contents: &'a str,
 ) -> Vec<&'a str> {
+
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents 
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
