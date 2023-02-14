a36ac6b9b3cb60ab3801eb39ae99ac0790e68ac2
Rick Hall <rickhall2000@hotmail.com>
2023-02-14 05:52:31 -0500

    Learning about smart pointers

--- Rust/the_rust_programming_language/smartpointer.rs
+++ Rust/the_rust_programming_language/smartpointer.rs
@@ -1,4 +1,5 @@
 use crate::List::{Cons, Nil};
+use crate::List2::{Cons2, Nil2};
 use std::ops::Deref;
 
 fn main() {
@@ -17,7 +18,29 @@ fn main() {
 
     deref_example();
     deref_coercion();
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    drop(c);
+    println!("CustomSmartPointers created.");
+
+    let a = Rc::new(Cons2(5, Rc::new(Cons2(10, Rc::new(Nil2)))));
+    let b = Cons2(3, Rc::clone(&a));
+    let c = Cons2(4, Rc::clone(&a));
+    println!("how many A's do we have? {}", Rc::strong_count(&a) );
 }
+
+enum List2 {
+    Cons2(i32, Rc<List2>),
+    Nil2,
+}
+
+use std::rc::Rc;
+
 
 fn box_example() {
     let b = Box::new(5);
@@ -65,4 +88,17 @@ fn hello(name: &str) {
 fn deref_coercion() {
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!(
+            "Dropping CustomSmartPointer with data `{}`!",
+            self.data
+        )
+    }
 }
