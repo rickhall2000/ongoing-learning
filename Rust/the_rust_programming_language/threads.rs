c30d7605e3a9c68c06218778c79246352ad86405
Rick Hall <rickhall2000@hotmail.com>
2023-02-17 07:08:09 -0500

    Learning about threads

--- Rust/the_rust_programming_language/threads.rs
+++ Rust/the_rust_programming_language/threads.rs
@@ -1,8 +1,31 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
+use std::sync::{Mutex, Arc};
 
 fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+
+}
+
+fn multi_channel() {
     let (tx, rx) = mpsc::channel();
     let tx1 = tx.clone();
 
