ee6459ae73a909d243c13346c26fb038b2be7699
Rick Hall <rickhall2000@hotmail.com>
2023-02-18 10:56:39 -0500

    add terraform example

---
+++ language-lessons/terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+
+  backend "s3" {
+    bucket = "rickslearning-terraform"
+    key    = "langage-learning-dev/terraform.tfstate"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
