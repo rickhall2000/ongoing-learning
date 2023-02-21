b94d52ebd7d379eecbc887105c26e07683395756
Rick Hall <rickhall2000@hotmail.com>
2023-02-20 19:46:43 -0500

    Started downloading and parsing

--- character-finder/src/downloader.py
+++ character-finder/src/downloader.py
@@ -2,11 +2,11 @@
 # This function will need to return the next page when there is a series of pages
 # this function will need to return the list of characters
 import urllib.request
+from bs4 import BeautifulSoup
 
-sample_url = "https://www.imdb.com/title/tt1288767/"
-#sample_url "https://rickslearning.com"
-next_url = "https://www.imdb.com/title/tt1288768/?ref_=tt_ep_nx"
-sample_file = "sample_page.html"
+clone_wars1_url = "https://www.imdb.com/title/tt1288767/"
+a_new_hope = "https://www.imdb.com/title/tt0076759/"
+sample_file = "star_wars_iv.html"
 
 def download_sample_page(url, file_path):
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -20,7 +20,18 @@ def get_page(url):
     with urllib.request.urlopen(req) as response:
         html = response.read().decode('utf-8')
     return html
-        
-        
+
+def find_next_page(html):
+    s = BeautifulSoup(html, 'html.parser')
+
+    next_link = s.find('a', title="Next episode")
+    return next_link['href'] if next_link else None
+
+def find_cast_link(html):
+    s = BeautifulSoup(html, 'html.parser')
+    cast_section = s.find('section', attrs={'data-testid': 'title-cast'})
+    cast_link = cast_section.select('a')[0]
+    return cast_link['href'] if cast_link else None
+    
 if __name__ == "__main__":
-    download_sample_page(sample_url, sample_file)
+    download_sample_page(a_new_hope, sample_file)
--- character-finder/src/test_downloader.py
+++ character-finder/src/test_downloader.py
@@ -1,9 +1,31 @@
 import unittest
+import downloader as d
 
-class test_downloader(unittest.TestCase):
-    def test_download(self):
-        self.assertEqual(1, 1)
-        
+star_wars_file = "star_wars_iv.html"
+clone_wars1_file = "clone_wars1.html"
+clone_wars2_url = "/title/tt1288768/?ref_=tt_ep_nx"
+
+
+class test_cast_link_finder(unittest.TestCase):
+    def test_find_clonewars_cast(self):
+        with open(clone_wars1_file, 'r', encoding='utf-8') as file:
+            html = file.read()
+        self.assertEqual('/title/tt1288767/fullcredits/cast/?ref_=tt_cl_sm', d.find_cast_link(html))
+    def test_find_starwars_cast(self):
+        with open(star_wars_file, 'r', encoding='utf-8') as file:
+            html = file.read()
+        self.assertEqual('/title/tt0076759/fullcredits/cast/?ref_=tt_cl_sm', d.find_cast_link(html))
+    
+class test_next_finder(unittest.TestCase):
+    def test_no_next_found_when_doesnt_exist(self):
+        with open(star_wars_file, 'r', encoding='utf-8') as file:
+            html = file.read()
+        self.assertEqual(None, d.find_next_page(html))
+    
+    def test_finds_next_when_does_exist(self):
+        with open(clone_wars1_file, 'r', encoding='utf-8') as file:
+            html = file.read()
+        self.assertEqual(clone_wars2_url, d.find_next_page(html))    
         
 if __name__ == '__main__':
     unittest.main()        
