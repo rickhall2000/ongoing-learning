b294b2f7f3baa3948e1ad954c5d671d8af3f87b3
Rick Hall <rickhall2000@hotmail.com>
2023-09-09 18:52:23 -0400

    following the builder pattern

---
+++ docker-workshop/ch4/builder-pattern/build.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+echo "Creating builder image"
+docker image build -t welcome-builder:v1 -f Dockerfile.build .
+docker container create --name welcome-builder-container welcome-builder:v1
+docker container cp welcome-builder-container:/myapp/welcome .
+docker container rm -f welcome-builder-container
+echo "Creating welcome runtime image"
+docker image build -t welcome-runtime:v1 .
+rm welcome
