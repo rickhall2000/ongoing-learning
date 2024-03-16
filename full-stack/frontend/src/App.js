8656ecd784019ecd09b60e49036772843150bbed
Rick Hall <rickhall2000@hotmail.com>
2024-03-16 12:17:08 -0400

    finished chapter 6

--- full-stack/frontend/src/App.js
+++ full-stack/frontend/src/App.js
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-const handleSearchSubmit = (e) => {
-  console.log(e);
-}
 
 function App() {
+  const [word, setWord] = useState('');
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  }  
+
   return (
     <div>
       <Header title="Images Gallery"/>
-      <Search handleSubmit={handleSearchSubmit}/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
