4573f62999ca78fd13652952c711271d829c2a9e
Rick Hall <rickhall2000@hotmail.com>
2024-03-16 14:37:50 -0400

    linter errors

--- full-stack/frontend/src/components/Header.js
+++ full-stack/frontend/src/components/Header.js
@@ -3,18 +3,17 @@ import { Navbar } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 
 const NavbarStyle = {
-    backgroundColor: "lightblue",
-    
+  backgroundColor: 'lightblue',
 };
 
-const Header = ({title}) => {
-    return (
-        <Navbar style={NavbarStyle} data-bs-theme="light">
-        <Container>
-          <Navbar.Brand href="/">{title}</Navbar.Brand>
-        </Container>
-      </Navbar>
-    )
+const Header = ({ title }) => {
+  return (
+    <Navbar style={NavbarStyle} data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
--- full-stack/frontend/src/components/Search.js
+++ full-stack/frontend/src/components/Search.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
-const Search = ({word, setWord, handleSubmit}) => {
-    return (
-        <Container className="mt-4">
-            <Row className='justify-content-center'>
-                <Col xs={12} md={8}>
-                    <Form onSubmit={handleSubmit}>
-                        <Row>
-                                <Col xs={9}>
-                                    <Form.Control
-                                        type="text" 
-                                        value={word}
-                                        onChange={(e) => setWord(e.target.value)}
-                                        placeholder="Search for new image..." />
-                                </Col>
-                                <Col>
-                                    <Button variant="primary" type="submit">Search</Button>
-                                </Col>
-                        </Row>
-                    </Form>
-                </Col>
+const Search = ({ word, setWord, handleSubmit }) => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8}>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col xs={9}>
+                <Form.Control
+                  type="text"
+                  value={word}
+                  onChange={(e) => setWord(e.target.value)}
+                  placeholder="Search for new image..."
+                />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">
+                  Search
+                </Button>
+              </Col>
             </Row>
-        </Container>
-    )
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Search;
