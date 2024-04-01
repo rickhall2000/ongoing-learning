36e6ac80f357df8d953e348fdee1aac2f86f286d
Rick Hall <rickhall2000@hotmail.com>
2024-04-01 08:20:26 -0400

    enable debug mode

--- full-stack/api/main.py
+++ full-stack/api/main.py
@@ -7,11 +7,14 @@ load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
 UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 if not UNSPLASH_KEY:
     raise EnvironmentError("Please create .env.local file and add UNSPLASH_KEY")
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = DEBUG
 
 @app.route('/new-image')
 def new_image():
