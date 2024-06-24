ef8587044e8652e35fd17704870dedd6142af31b
Rick Hall <rickhall2000@hotmail.com>
2024-06-24 09:00:10 -0400

    add linting to python project

--- full-stack/api/main.py
+++ full-stack/api/main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path="./.env.local")
 
 UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
-UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
 DEBUG = bool(os.environ.get("DEBUG", False))
 
 if not UNSPLASH_KEY:
@@ -16,22 +16,19 @@ if not UNSPLASH_KEY:
 app = Flask(__name__)
 CORS(app)
 
-app.config['DEBUG'] = DEBUG
+app.config["DEBUG"] = DEBUG
 
-@app.route('/new-image')
+
+@app.route("/new-image")
 def new_image():
-    word = request.args.get('query')
-    headers = {
-        "Accept-Version": "v1",
-        "Authorization": f"Client-ID {UNSPLASH_KEY}"
-    }
-    params = {
-        "query": word
-    }
+    word = request.args.get("query")
+    headers = {"Accept-Version": "v1", "Authorization": f"Client-ID {UNSPLASH_KEY}"}
+    params = {"query": word}
 
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050)
